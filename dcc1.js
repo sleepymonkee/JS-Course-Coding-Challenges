ce56239a364816d3dcaef6023e2af3cb0bbece5c
sleepymonkee <sena.kristiawan0@gmail.com>
2024-10-11 11:18:45 +0700

    dcc1

---
+++ dcc1.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const arr = [17, 21, 23];
+// const arr = [12, 5, -5, 0, 4];
+const printForecast = function(arr){
+    for(let i = 0 ; i<arr.length ; i++){
+        console.log(`${arr[i]}C in ${i+1} days`);
+    }
+
+}
+
+printForecast(arr);
