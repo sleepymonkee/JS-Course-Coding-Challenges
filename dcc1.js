4977f3a7575e6d562cd7038467f889821306fae3
sleepymonkee <sena.kristiawan0@gmail.com>
2024-10-16 12:36:13 +0700

    prettier doang wkwk

--- dcc1.js
+++ dcc1.js
@@ -1,20 +1,37 @@
-'use strict';
+"use strict";
+
+// Sub Problem
+/*
+    1. What is X days in ...temp in X Days?
+    Answer :
+        index+1
+    2. What type of array?
+    Answer : 
+        Number, float;
+    3. What is in the array?
+    Answer :
+        Temp
+    4. What is the output?
+    Answer :
+        17C in 1 days
+        21C in 2 days
+        23C in 3 days
+*/
 
 const arr = [17, 21, 23];
 // const arr = [12, 5, -5, 0, 4];
-const printForecast = function(arr){
-    for(let i = 0 ; i<arr.length ; i++){
-        console.log(`${arr[i]}C in ${i+1} days`);
-    }
+const printForecast = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(`${arr[i]}C in ${i + 1} days`);
+  }
+};
 
-}
-
-function printForecast2(arr){
-    let i = 0;
-    while(i<arr.length){
-        console.log(`${arr[i]}C in ${i+1} days`);
-        i++;
-    }
+function printForecast2(arr) {
+  let i = 0;
+  while (i < arr.length) {
+    console.log(`${arr[i]}C in ${i + 1} days`);
+    i++;
+  }
 }
 
 printForecast(arr);
